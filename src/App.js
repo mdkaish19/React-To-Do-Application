2ffd66b5901857f7354b55540068821404c93143
mdkaish19 <mdkaish1999@gmail.com>
2024-05-15 19:06:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import TaskInput from './components/TaskInput';
+import TaskList from './components/TaskList';
+import { Provider } from 'react-redux';
+import store from './store';
+import './App.css'; // Import CSS file
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="container">
+        <h1>React To-Do Application</h1>
+        <TaskInput />
+        <TaskList />
+      </div>
+    </Provider>
   );
 }
 
