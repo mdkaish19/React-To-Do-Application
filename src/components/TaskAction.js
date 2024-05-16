7f93a016fc953e500d5b3b4e3de6ccc3c1d91105
mdkaish19 <mdkaish1999@gmail.com>
2024-05-17 02:15:14 +0530

    New Update

--- src/components/TaskAction.js
+++ src/components/TaskAction.js
@@ -1,13 +1,20 @@
 export const addTask = (text) => ({
-    type: 'ADD_TASK',
-    payload: {
-      id: Math.random(),
-      text,
-    },
-  });
-  
-  export const deleteTask = (id) => ({
-    type: 'DELETE_TASK',
-    payload: id,
-  });
-  
+  type: 'ADD_TASK',
+  payload: {
+    id: Math.random(),
+    text,
+  },
+});
+
+export const deleteTask = (id) => ({
+  type: 'DELETE_TASK',
+  payload: id,
+});
+
+export const updateTask = (id, updatedText) => ({
+  type: 'UPDATE_TASK',
+  payload: {
+    id,
+    updatedText,
+  },
+});
--- src/components/TaskReducer.js
+++ src/components/TaskReducer.js
@@ -1,24 +1,30 @@
 // src/reducers/taskReducer.js
 const initialState = {
-    tasks: [],
-  };
-  
-  const taskReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case 'ADD_TASK':
-        return {
-          ...state,
-          tasks: [...state.tasks, action.payload],
-        };
-      case 'DELETE_TASK':
-        return {
-          ...state,
-          tasks: state.tasks.filter((task) => task.id !== action.payload),
-        };
-      default:
-        return state;
-    }
-  };
-  
-  export default taskReducer;
-  
+  tasks: [],
+};
+
+const taskReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADD_TASK':
+      return {
+        ...state,
+        tasks: [...state.tasks, action.payload],
+      };
+    case 'DELETE_TASK':
+      return {
+        ...state,
+        tasks: state.tasks.filter((task) => task.id !== action.payload),
+      };
+    case 'UPDATE_TASK':
+      return {
+        ...state,
+        tasks: state.tasks.map((task) =>
+          task.id === action.payload.id ? { ...task, text: action.payload.updatedText } : task
+        ),
+      };
+    default:
+      return state;
+  }
+};
+
+export default taskReducer;
